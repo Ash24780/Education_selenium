8e2322ef6dfc84f014a5d796f493f645f97813d5
Ashutosh Upadhyay <ashutoshup@gmail.com>
2019-05-30 03:25:00 +0100

    FIRST COMMIT

---
+++ src/module1/FirstJavaClassFile.java
@@ -0,0 +1,17 @@
+package module1;
+
+import org.openqa.selenium.WebDriver;
+
+public class FirstJavaClassFile {
+
+	public static void main(String[] args) {
+		
+ WebDriver Driver = new Chrome Driver ()
+	}
+
+	private static void Driver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
